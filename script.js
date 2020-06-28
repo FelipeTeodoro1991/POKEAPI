b6167425440e96af535be6e00981674319e5c8bd
Felipe.teodoro1991 <fteodoro1991@gmail.com>
2020-06-28 16:45:19 -0300

    Inicialização de arquvi json

--- script.js
+++ script.js
@@ -4,23 +4,21 @@ function pegaValorDiditado() {
     valor = document.getElementById("valordigitado").value;
     return valor
 }
+let nome = "";
+let habilidade = "";
+let tipo = "";
+let id = "";
 
 async function pegaNomePokemon() {
     axios.get(`https://pokeapi.co/api/v2/pokemon/${valor}`)
         .then(async function main(resposta) {
             let dados = await resposta.data;
             let nomePokemon = await dados.name;
+            nome.push(nomePokemon);
             document.getElementById("nomedopokemon").innerHTML = `${nomePokemon.toUpperCase()}`;
             let lista = document.querySelector("#habilidade");
             let habilidade = dados.abilities.map( abili => `<p>${ abili.ability.name}</p>`);
             lista.innerHTML = habilidade.join('')
-            // habilidade.forEach((hb, i) => {
-            //     let habilidades = hb.ability.name;
-            //     let elementoPai =  document.getElementById("habilidade").innerHTML = `<p>${habilidades}</>`
-            //     let criaTag = document.createElement('p')
-            //     let criartexto = criaTag.innerHTML=`${habilidades}`;
-            //     elementoPai.appendChild(criaTag);
-            // });
             let tipo = await dados.types
             tipo.forEach((tp, i) => {
                 let tipos = tp.type.name
@@ -33,6 +31,21 @@ async function pegaNomePokemon() {
         })
 }
 
+console.log(nome)
 
+// var fs = 'readFileSync';
+var fs = 'pokeagenda.json';
+// var arquivoSerializado = fs.readFileSync(caminhoDoArquivo);
+var arquivoDeserializado =[];
+// arquivoDeserializado.push(JSON.parse(arquivoSerializado));
 
-// pegaNomePokemon()
+console.log(arquivoDeserializado);
+
+var pokeBola = {} //cria objeto carro 
+pokeBola.nome = nome //istancia cor como propriedade do pokeBola
+pokeBola.habilidade = habilidade //istancia modelo como propriedade do pokeBola
+pokeBola.tipo = tipo //istancia marca como propriedade do carro  
+pokeBola.id = id //istancia o id do pokeBola
+var objetoSerializado = JSON.stringify(pokeBola) // serializa o arquivo
+fs.writeFileSync(caminhoDoArquivo,objetoSerializado)// parametro 1 informa a pasta destino, parametro 2 informa o arquivo a ser enviado
+
